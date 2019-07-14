c9808f30ec98f4321243662bb8b9270c3b535917
Gabriel <gabriel@donadora.mx>
2019-07-14 17:25:27 -0500

    definiendo rutas para trabajar con los customers y su wallet.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope module: 'api', path: 'api' do
-    resources :customers
+    resources :customers do
+      resources :credit_cards
+      resources :backs
+      resources :transactions
+    end
   end
 end
