ff9b354d55927435e4d728d095a8caf45996e313
Gabriel <gabriel@donadora.mx>
2019-07-14 17:13:22 -0500

    routes para customers.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope module: 'api', path: 'api' do
+    resources :customers
+  end
 end
