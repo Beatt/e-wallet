8cab1bda06ebb9567a2d66fc08111e417fe1acdb
Gabriel <gabriel@donadora.mx>
2019-07-14 19:01:45 -0500

    creo servicio encargado de crear un customer y se incluye specs del servicio.

---
+++ spec/services/create_customer_services_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'Customer' do
+  context 'Create customer' do
+
+    subject(:params) { { name: 'Gabriel', email: 'ggabriel@gmail.com' }}
+
+    it 'should create customer successfully' do
+      customer = CreateCustomerServices.new(params).create
+      expect(customer).not_to be_a_new(Customer)
+    end
+
+    it 'should create number account' do
+      customer = CreateCustomerServices.new(params).create
+      expect(customer.number_account.to_i.is_a? Integer).to be_truthy
+    end
+
+    it 'shouldn´t create a new customer with the same email' do
+      customer_1 = CreateCustomerServices.new(params).create
+      customer_2 = CreateCustomerServices.new(params).create
+
+      expect(customer_1).not_to be_a_new(Customer)
+      expect(customer_2).to eq(customer_1)
+    end
+
+    it 'should show errors when customer save failed' do
+      customer = CreateCustomerServices.new(name: 'Juan', email: nil).create
+      expect(customer).to eq({email: ["can't be blank"]})
+    end
+
+  end
+end
