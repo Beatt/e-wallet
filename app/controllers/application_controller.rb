405866a87eb91e6859ae42278403f4b55d6b913e
Gabriel <gabriel@donadora.mx>
2019-07-17 23:33:03 -0500

    home.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def home
+    render json: 'e-wallet por Gabriel G.'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'application#home'
   scope module: 'api', path: 'api' do
     resources :customers, except: [:destroy] do
       resources :credit_cards
