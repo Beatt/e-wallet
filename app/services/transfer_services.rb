5fa4be29153a5701b59a0828deb2b3c88c50f1e5
Gabriel <gabriel@donadora.mx>
2019-07-17 21:34:00 -0500

    refactorizando...

--- app/services/transfer_services.rb
+++ app/services/transfer_services.rb
@@ -9,12 +9,16 @@ class TransferServices
     return ['Sin fondos'] if @params[:value_in_cents].to_f > customer.balance
     transfer = Back::Transfer.new(@params)
     return transfer.errors.full_messages unless transfer.save
-    tax = Tax.between_minimum_limit_value(@params[:value_in_cents].to_f)
-    GeneralAccount.create(back_id: transfer.id, taxe_id: tax.id)
+    create_general_account(transfer)
     transfer
   end
 
   private
+
+  def create_general_account(transfer)
+    tax = Tax.between_minimum_limit_value(@params[:value_in_cents].to_f)
+    GeneralAccount.create(back_id: transfer.id, taxe_id: tax.id)
+  end
 
   def customer
     Customer.find(@params[:customer_id])
