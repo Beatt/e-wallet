fe372f387b1ed1bd74b58ee988ac8dbdf57d6a1f
Gabriel <gabriel@donadora.mx>
2019-07-20 17:16:42 -0500

    descontando comisión.

--- app/services/transfer_services.rb
+++ app/services/transfer_services.rb
@@ -8,13 +8,17 @@ class TransferServices
   def process
     return ['Sin fondos para transferir'] if @params[:value_in_cents].to_f > @customer.try(:balance)
     assign_account_recipient
-    transfer = Back::Transfer.new(@params)
-    return transfer.errors.full_messages unless transfer.save
-    create_general_account(transfer)
-    transfer
+    apply_tax
+    create_transfer
+    return @transfer.errors.full_messages unless @transfer.save
+    create_general_account
   end
 
   private
+
+  def create_transfer
+    @transfer ||= Back::Transfer.new(@params)
+  end
 
   def assign_account_recipient
     @params[:account_recipient] = customer_recipient.try(:id)
@@ -24,8 +28,14 @@ class TransferServices
     Customer.find_by(account_number: @params[:account_recipient])
   end
 
-  def create_general_account(transfer)
+  def apply_tax
     tax = Tax.between_minimum_limit_value(@params[:value_in_cents].to_f)
-    GeneralAccount.create(back_id: transfer.id, taxe_id: tax.id)
+    @params[:value_in_cents] = (@params[:value_in_cents] * tax.percentage) + tax.fixed_rate
+  end
+
+  def create_general_account
+    tax = Tax.between_minimum_limit_value(@params[:value_in_cents].to_f)
+    GeneralAccount.create(back_id: @transfer.id, taxe_id: tax.id)
+    @transfer
   end
 end
