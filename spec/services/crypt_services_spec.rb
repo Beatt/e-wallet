c5ab7b6a313349f958de9113ba1b5ae6bd5ff868
Gabriel <gabriel@donadora.mx>
2019-07-21 16:43:04 -0500

    refactorizando...

--- spec/services/crypt_services_spec.rb
+++ spec/services/crypt_services_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Crypt services' do
-  context 'encrypt' do
-    it 'should encrypt data successfully' do
+  context 'when encrypt data successfully' do
+    it 'should response with data encrypt' do
       data = '302030402'
       data_encrypt = CryptServices.encrypt(data)
       expect(data_encrypt).not_to eq(data)
     end
   end
 
-  context 'decrypt' do
-    it 'should decrypt data successfully' do
+  context 'when decrypt data successfully' do
+    it 'should response with data decrypy' do
       data = '302030402'
       data_encrypt = CryptServices.encrypt(data)
       data_decrypt = CryptServices.decrypt(data_encrypt)
